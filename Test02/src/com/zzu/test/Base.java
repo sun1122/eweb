80b972a2a90979f2b72835d0aeebd013fcdad678
zjs <282581063@qq.com>
2018-08-25 13:13:51 +0800

    测试修改

--- Test02/src/com/zzu/test/Base.java
+++ Test02/src/com/zzu/test/Base.java
@@ -1,13 +1,19 @@
 package com.zzu.test;
-
+/**
+ * 测试
+ * @author Administrator
+ *
+ */
 public class Base {
-private String baseName = "base";
-public Base(){
-	System.out.println("--1--");
-callName();
-System.out.println("--2--");
-    }
-public void callName(){
-System.out.println(baseName);
-    }
+	private String baseName = "base";
+
+	public Base() {
+		System.out.println("--1--");
+		callName();
+		System.out.println("--2--");
+	}
+
+	public void callName() {
+		System.out.println(baseName);
+	}
 }
